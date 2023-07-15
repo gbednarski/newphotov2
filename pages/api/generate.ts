d81fd0a0500129d867c8a67e9bb793cbf1bdaeba
gbednarski <139589791+gbednarski@users.noreply.github.com>
2023-07-15 10:36:44 +0200

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -58,28 +58,25 @@ export default async function handler(
   let endpointUrl = jsonStartResponse.urls.get;
 
   // GET request to get the status of the image restoration process & return the result when it's ready
-  let restoredImage: string | null = null;
-  while (!restoredImage) {
-    // Loop in 1s intervals until the alt text is ready
-    console.log("polling for result...");
-    let finalResponse = await fetch(endpointUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Token " + process.env.REPLICATE_API_KEY,
-      },
-    });
-    let jsonFinalResponse = await finalResponse.json();
+let restoredImage: string | null = null;
+while (!restoredImage) {
+  // Loop in 1s intervals until the alt text is ready
+  console.log("polling for result...");
+  let finalResponse = await fetch(endpointUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Token " + process.env.REPLICATE_API_KEY,
+    },
+  });
+  let jsonFinalResponse = await finalResponse.json();
 
-    if (jsonFinalResponse.status === "succeeded") {
-      restoredImage = jsonFinalResponse.output;
-    } else if (jsonFinalResponse.status === "failed") {
-      break;
-    } else {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
+  if (jsonFinalResponse.status === "succeeded") {
+    restoredImage = jsonFinalResponse.output;
+  } else if (jsonFinalResponse.status === "failed") {
+    break;
+  } else {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
   }
-  res
-    .status(200)
-    .json(restoredImage ? restoredImage : "Failed to restore image");
 }
+res.status(200).json(restoredImage ? restoredImage : "Failed to restore image");
